a2efbbd46202302f0bcb0a6df93524057b517460
Manwel Bugeja <manwelbugeja@Manwels-MBP.lan>
2022-04-15 10:14:16 +0200

    Added webview example

---
+++ WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.webviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        // opens new links in the webview instead of browser
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.wikipedia.org");
+    }
+}
