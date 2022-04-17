4b70f2f9e877840fe78dd6bcd8c3ad873e810deb
Manwel Bugeja <manwelbugeja@Manwels-MBP.lan>
2022-04-17 11:22:11 +0200

    Finished WebView example

--- WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
+++ WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -2,25 +2,63 @@ package com.example.webviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
+
         myWebView.getSettings().setLoadsImagesAutomatically(true);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
+
         // opens new links in the webview instead of browser
-        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if (request.getUrl().toString().contains("wikipedia")) {
+
+                    return super.shouldOverrideUrlLoading(view, request);
+                }else{
+                    try {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.valueOf(request.getUrl())));
+                        startActivity(browserIntent);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                view.loadUrl("javascript:alert('Hello World');");
+            }
+        });
+
         myWebView.loadUrl("https://www.wikipedia.org");
+
     }
 }
